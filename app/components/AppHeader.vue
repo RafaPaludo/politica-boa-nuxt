ab5b0674b65bbd4eb7ac3f16de70e13c9d64ffa6
RafaPaludo <rafael.paludo@franq.com.br>
2025-03-13 20:18:11 -0300

    feat: use nuxt ui pro 3 sass template

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,31 +1,51 @@
 <script setup lang="ts">
-const nuxtApp = useNuxtApp()
-const { activeHeadings, updateHeadings } = useScrollspy()
+import type { NavItem } from '@nuxt/content'
 
-const links = computed(() => [{
-  label: 'FAQ',
-  to: '#faq',
-  icon: 'i-heroicons-question-mark-circle',
-  active: activeHeadings.value.includes('faq')
-}])
+const navigation = inject<Ref<NavItem[]>>('navigation', ref([]))
 
-nuxtApp.hooks.hookOnce('page:finish', () => {
-  updateHeadings([
-    document.querySelector('#faq')
-  ])
-})
+const links = [{
+  label: 'Docs',
+  to: '/docs'
+}, {
+  label: 'Pricing',
+  to: '/pricing'
+}, {
+  label: 'Blog',
+  to: '/blog'
+}]
 </script>
 
 <template>
   <UHeader :links="links">
     <template #logo>
-      <span class="text-primary">
-        IPB
-      </span>
+      Nuxt UI Pro <UBadge
+        label="SaaS"
+        variant="subtle"
+        class="mb-0.5"
+      />
+    </template>
+
+    <template #right>
+      <UButton
+        label="Sign in"
+        color="gray"
+        to="/login"
+      />
+      <UButton
+        label="Sign up"
+        icon="i-heroicons-arrow-right-20-solid"
+        trailing
+        color="black"
+        to="/signup"
+        class="hidden lg:flex"
+      />
     </template>
 
     <template #panel>
-      <UAsideLinks :links="links" />
+      <UNavigationTree
+        :links="mapContentNavigation(navigation)"
+        default-open
+      />
     </template>
   </UHeader>
 </template>
